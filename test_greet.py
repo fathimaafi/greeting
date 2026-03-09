f78dbdf5c163756b8f6ccbdbac1403b71d553031
Fathima <fathima.afi876@gmail.com>
2026-03-09 13:21:21 +0530

    Updated test file

--- test_greet.py
+++ test_greet.py
@@ -5,8 +5,8 @@ class TestGreet(unittest.TestCase):
     def test_greet_with_valid_name(self):
         self.assertEqual(greet("Alice"), "Hello, Alice!")
 
-    def test_greet_with_empty_string(self):
-        self.assertEqual(greet(" "), "Hello, !")
+    def test_test_greet_with_empty_string(self):
+        self.assertEqual(greet(""), "Hello, !")
 
 if __name__ == "__main__":
     unittest.main()
